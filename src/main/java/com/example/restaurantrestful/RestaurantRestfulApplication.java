cca9bb95fb5e0b35b39905ac66ef15cb52aea8d5
Erbil Colak <erbilcolak90@gmail.com>
2024-03-29 21:52:36 +0300

    SRR-101-ingredient-entity-class-defined-type-and-unit-enums-created

--- src/main/java/com/example/restaurantrestful/RestaurantRestfulApplication.java
+++ src/main/java/com/example/restaurantrestful/RestaurantRestfulApplication.java
@@ -1,4 +1,4 @@
-package com.example.restfulrestaurant;
+package com.example.restaurantrestful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/restaurantrestful/enums/UnitTypeEnums.kt
@@ -0,0 +1,11 @@
+package com.example.restaurantrestful.enums
+
+enum class UnitTypeEnums {
+
+    KG,
+    GR,
+    OZ,
+    LT,
+    MIL,
+
+}
