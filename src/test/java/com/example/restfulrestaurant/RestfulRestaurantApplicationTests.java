617ca152342e55aab196a41352f6f2754602c952
Erbil Colak <erbilcolak90@gmail.com>
2024-03-29 17:44:23 +0300

    initial commit

---
+++ src/test/java/com/example/restfulrestaurant/RestfulRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
