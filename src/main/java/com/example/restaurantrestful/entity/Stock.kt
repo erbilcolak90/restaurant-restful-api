2e8672d53f80a265c6ca147a24a03baab90851ce
Erbil Colak <erbilcolak90@gmail.com>
2024-04-02 13:00:10 +0300

    SRR-110-stock-entity-defined

---
+++ src/main/java/com/example/restaurantrestful/entity/Stock.kt
@@ -0,0 +1,25 @@
+package com.example.restaurantrestful.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.springframework.data.elasticsearch.annotations.Document
+import org.springframework.data.elasticsearch.annotations.Field
+import org.springframework.data.elasticsearch.annotations.FieldType
+import java.util.*
+
+@Entity
+@Document(indexName = "stock")
+data class Stock @JvmOverloads constructor(
+
+        @Id
+        var id: String,
+        @Field(type = FieldType.Keyword)
+        var ingredientId: String,
+        @Field(type = FieldType.Keyword)
+        var type: String ,
+        @Field(type = FieldType.Keyword)
+        var unit: String,
+        @Field(type= FieldType.Double)
+        var quantity: Double,
+        @Field(type= FieldType.Date)
+        var expireDate: Date =Date()): BaseEntity()
