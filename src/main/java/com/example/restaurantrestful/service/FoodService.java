245676320a7e8feacd0561d98ceed7d1e0ae9c02
Erbil Colak <erbilcolak90@gmail.com>
2024-04-06 13:53:14 +0300

    SRR-131-getFoodById-method-defined-service-and-test-classes-and-custom-exception-defined

--- src/main/java/com/example/restaurantrestful/service/FoodService.java
+++ src/main/java/com/example/restaurantrestful/service/FoodService.java
@@ -1,5 +1,7 @@
 package com.example.restaurantrestful.service;
 
+import com.example.restaurantrestful.entity.Food;
+import com.example.restaurantrestful.exception.CustomException;
 import com.example.restaurantrestful.repository.jpa.FoodRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,9 @@ public class FoodService {
     public FoodService(FoodRepository foodRepository, RecipeService recipeService) {
         this.foodRepository = foodRepository;
         this.recipeService = recipeService;
+    }
+
+    public Food getFoodById(String id){
+        return foodRepository.findById(id).orElseThrow(CustomException::foodNotFound);
     }
 }
