0eae375ae810e0132ef2d888925be0406093cc2d
Erbil Colak <erbilcolak90@gmail.com>
2024-04-11 22:21:06 +0300

    SRR-157-getOrderById-method-defined-in-service-and-test-classes-and-custom-exception-defined

--- src/main/java/com/example/restaurantrestful/service/OrderService.java
+++ src/main/java/com/example/restaurantrestful/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.example.restaurantrestful.service;
 
+import com.example.restaurantrestful.entity.Order;
+import com.example.restaurantrestful.exception.CustomException;
 import com.example.restaurantrestful.repository.jpa.OrderRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,9 @@ public class OrderService {
     public OrderService(OrderRepository orderRepository, OrderProductService orderProductService) {
         this.orderRepository = orderRepository;
         this.orderProductService = orderProductService;
+    }
+
+    public Order getOrderById(String id){
+        return orderRepository.findById(id).orElseThrow(CustomException::orderNotFound);
     }
 }
--- src/test/java/com/example/restaurantrestful/OrderServiceTest.java
+++ src/test/java/com/example/restaurantrestful/OrderServiceTest.java
@@ -1,17 +1,24 @@
 package com.example.restaurantrestful;
 
 import com.example.restaurantrestful.entity.Order;
+import com.example.restaurantrestful.exception.CustomException;
 import com.example.restaurantrestful.repository.jpa.OrderRepository;
 import com.example.restaurantrestful.service.OrderProductService;
 import com.example.restaurantrestful.service.OrderService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderServiceTest {
@@ -20,7 +27,7 @@ public class OrderServiceTest {
     private OrderService orderServiceMock;
 
     @Mock
-    private OrderRepository orderRepository;
+    private OrderRepository orderRepositoryMock;
 
     @Mock
     private OrderProductService orderProductServiceMock;
@@ -35,6 +42,24 @@ public class OrderServiceTest {
         orderMock.setTotalPrice(100.0);
         orderMock.setCompleted(false);
 
+    }
+    @DisplayName("getOrderById should return valid order when given id is exist")
+    @Test
+    void testGetOrderById_success(){
+        String test_id = "test_id";
+
+        when(orderRepositoryMock.findById(test_id)).thenReturn(Optional.ofNullable(orderMock));
+
+        Order result = orderServiceMock.getOrderById(test_id);
+
+        assertNotNull(result);
+
+    }
+
+    @DisplayName("getOrderById should throw custom exception orderNotFound when given id does not exist")
+    @Test
+    void testGetOrderById_orderNotFound(){
+        assertThrows(CustomException.class,()-> orderServiceMock.getOrderById(anyString()));
     }
 
     @AfterEach
