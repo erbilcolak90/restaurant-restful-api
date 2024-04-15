138c207cf700090189c385aa92a3ca84b7404684
Erbil Colak <erbilcolak90@gmail.com>
2024-04-15 09:05:20 +0300

    SRR-171-getAllOrdersByIsCompletedFalse-method-defined-in-service-and-test-classes

--- src/main/java/com/example/restaurantrestful/repository/jpa/OrderRepository.java
+++ src/main/java/com/example/restaurantrestful/repository/jpa/OrderRepository.java
@@ -2,6 +2,12 @@ package com.example.restaurantrestful.repository.jpa;
 
 import com.example.restaurantrestful.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, String > {
+
+    @Query("SELECT * FROM Orders WHERE is_completed = false")
+    List<Order> findByIsCompletedFalse();
 }
