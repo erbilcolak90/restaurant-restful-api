26bb334a11237f63d0d9f56306febbf37281bf18
Erbil Colak <erbilcolak90@gmail.com>
2024-04-16 11:54:55 +0300

    SRR-184-getAllMenus-method-defined-in-service-test-classes

--- src/test/java/com/example/restaurantrestful/MenuServiceTest.java
+++ src/test/java/com/example/restaurantrestful/MenuServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.restaurantrestful;
 
+import com.example.restaurantrestful.dto.payloads.MenuPayload;
 import com.example.restaurantrestful.entity.Menu;
 import com.example.restaurantrestful.exception.CustomException;
 import com.example.restaurantrestful.repository.elastic.MenuRepository;
@@ -13,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,14 +40,14 @@ public class MenuServiceTest {
 
     }
 
-    @DisplayName("getMenuById should return valid menu when given id is exist")
+    @DisplayName("getMenuById should return valid menuPayload when given id is exist")
     @Test
     void testGetMenuById_success(){
         String test_id = "test_id";
 
         when(menuRepositoryMock.findById(test_id)).thenReturn(Optional.ofNullable(menuMock));
 
-        Menu result = menuServiceMock.getMenuById(test_id);
+        MenuPayload result = menuServiceMock.getMenuById(test_id);
 
         assertNotNull(result);
     }
@@ -56,6 +59,32 @@ public class MenuServiceTest {
         when(menuRepositoryMock.findById(anyString())).thenReturn(Optional.empty());
 
         assertThrows(CustomException.class, ()-> menuServiceMock.getMenuById(anyString()));
+    }
+
+    @DisplayName("getAllMenus should return list menuPayload")
+    @Test
+    void testGetAllMenu_success(){
+        Menu menu1 = new Menu();
+        Menu menu2 = new Menu();
+        Menu menu3 = new Menu();
+        menu1.setId("test_menu_id_1");
+        menu1.setName("test_menu_name_1");
+        menu2.setId("test_menu_id_2");
+        menu2.setName("test_menu_name_2");
+        menu3.setId("test_menu_id_3");
+        menu3.setName("test_menu_name_3");
+        List<Menu> menuList = new ArrayList<>();
+        menuList.add(menu1);
+        menuList.add(menu2);
+        menuList.add(menu3);
+
+        when(menuRepositoryMock.findAll()).thenReturn(menuList);
+
+        List<MenuPayload> result = menuServiceMock.getAllMenus();
+
+        assertNotNull(result);
+        assertEquals(3,result.size());
+
     }
 
     @AfterEach
